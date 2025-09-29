015e50d86d72f2e6aea9212e856b2600d66e16f0
Ricardo <ricardo.rivera.alv@gmail.com>
2025-09-29 13:48:41 -0700

    NaranjaCommit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <title>Mi Web Service</title>
+      <style>
+        body {
+          /* Usamos Flexbox para centrar vertical y horizontalmente */
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+          background-color: #ff7b00ff;
+          font-family: sans-serif;
+        }
+        h1 {
+          color: deepseablue;
+          font-size: 3rem;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Lo balatro sale caro</h1>
+    </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
